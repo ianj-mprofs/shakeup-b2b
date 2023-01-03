8ed0e1d9beff331c7a1699a1311dd3d49ac52785
Ian Jackson <ianj@marketingprofs.com>
2023-01-03 16:47:05 -0700

    overview page

---
+++ assets/js/mobile-nav.js
@@ -0,0 +1,14 @@
+$("#ham-menu").click(function() {
+    $("#ham-menu").toggleClass('ham-menu-x');
+    $("#mobile-nav").toggleClass('mobile-nav-slide')
+});
+
+// const hamburger = document.getElementById('ham-menu');
+// const sideNav = document.getElementById('side-nav');
+
+// function openNav() {
+//     hamburger.classList.toggle('ham-menu-x');
+//     sideNav.classList.toggle('side-nav-slide')
+// };
+
+// hamburger.addEventListener('click', openNav);
