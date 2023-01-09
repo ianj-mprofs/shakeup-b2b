13a2e41e0437fbcc3d4e56daeb5e0852ed1af8e6
Ian Jackson <ianj@marketingprofs.com>
2023-01-09 11:28:46 -0700

    dropping copy into hero image and adjusting responsiveness

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,6 +1,7 @@
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
 
+// Controls where on screen animation activates
 const appearOptions = {
     rootMargin: "0px 0px -200px 0px"
 };
